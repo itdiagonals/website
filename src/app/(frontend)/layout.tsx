01e5f44c0b63269b9bc8e0e3efd691119aaa7307
ayeah <187853299+ayashar@users.noreply.github.com>
2026-03-20 21:02:10 +0700

    init(fonts): add fonts

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,17 +1,61 @@
 import React from 'react'
-import './styles.css'
+import './globals.css'
+import localFont from 'next/font/local'
+import { Inter } from 'next/font/google'
 
 export const metadata = {
   description: 'A blank template using Payload in a Next.js app.',
   title: 'Payload Blank Template',
 }
 
+const dorivalUITrial = localFont({
+  src: [
+    {
+      path: '../../../public/fonts/DorivalUITrial-Regular.otf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../../public/fonts/DorivalUITrial-Bold.otf',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-dorivaluitrial',
+})
+
+const dorivalUINrwTrial = localFont({
+  src: [
+    {
+      path: '../../../public/fonts/DorivalUINrwTrial-Regular.otf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../../public/fonts/DorivalUINrwTrial-Medium.otf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../../../public/fonts/DorivalUINrwTrial-Bold.otf',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-dorivaluinrwtrial',
+})
+
+const inter = Inter({
+  variable: '--font-inter',
+  subsets: ['latin'],
+})
+
 export default async function RootLayout(props: { children: React.ReactNode }) {
   const { children } = props
 
   return (
     <html lang="en">
-      <body>
+      <body className={`${dorivalUITrial.variable} ${dorivalUINrwTrial.variable} ${inter.variable} antialiased`}>
         <main>{children}</main>
       </body>
     </html>
