a96783fa173abf42393f77abf88b1c73cd0c9778
ayeah <187853299+ayashar@users.noreply.github.com>
2026-05-15 19:20:56 +0700

    ref(payload): remove payload from dependencies

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,17 +1,17 @@
 import { headers as getHeaders } from 'next/headers.js'
 import Image from 'next/image'
-import { getPayload } from 'payload'
+// import { getPayload } from 'payload'
 import React from 'react'
 import { fileURLToPath } from 'url'
 
-import config from '@/payload.config'
+// import config from '@/payload.config'
 import './globals.css'
 
 export default async function HomePage() {
-  const headers = await getHeaders()
-  const payloadConfig = await config
-  const payload = await getPayload({ config: payloadConfig })
-  const { user } = await payload.auth({ headers })
+  // const headers = await getHeaders()
+  // const payloadConfig = await config
+  // const payload = await getPayload({ config: payloadConfig })
+  // const { user } = await payload.auth({ headers })
 
   const fileURL = `vscode://file/${fileURLToPath(import.meta.url)}`
 
@@ -27,17 +27,17 @@ export default async function HomePage() {
             width={65}
           />
         </picture>
-        {!user && <h1>Welcome to your new project.</h1>}
-        {user && <h1>Welcome back, {user.email}</h1>}
+        {/* {!user && <h1>Welcome to your new project.</h1>} */}
+        {/* {user && <h1>Welcome back, {user.email}</h1>} */}
         <div className="links">
-          <a
+          {/* <a
             className="admin"
             href={payloadConfig.routes.admin}
             rel="noopener noreferrer"
             target="_blank"
           >
             Go to admin panel
-          </a>
+          </a> */}
           <a
             className="docs"
             href="https://payloadcms.com/docs"
